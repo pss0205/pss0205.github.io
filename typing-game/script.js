132b048a078065fba7605d98bdb78981e263d0ce
pss0205 <pss070205@gmail.com>
2022-01-23 09:58:33 -0500

    add typing game

---
+++ typing-game/script.js
@@ -0,0 +1,46 @@
+// inside script.js
+// all of our quotes
+const quotes = [
+    'apple banana',
+    'Coding is fun'
+];
+// store the list of words and the index of the word the player is currently typing
+let words = [];
+let wordIndex = 0;
+// the starting time
+let startTime = Date.now();
+// page elements
+const quoteElement = document.getElementById('quote');
+const messageElement = document.getElementById('message');
+const typedValueElement = document.getElementById('typed-value');
+
+// at the end of script.js
+document.getElementById('start').addEventListener('click', () => {
+    // get a quote
+    const quoteIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[quoteIndex];
+    // Put the quote into an array of words
+    words = quote.split(' ');
+    // reset the word index for tracking
+    wordIndex = 0;
+  
+    // UI updates
+    // Create an array of span elements so we can set a class
+    const spanWords = words.map(function(word) { return `<span>${word} </span>`});
+    // Convert into string and set as innerHTML on quote display
+    quoteElement.innerHTML = spanWords.join('');
+    // Highlight the first word
+    quoteElement.childNodes[0].className = 'highlight';
+    // Clear any prior messages
+    messageElement.innerText = '';
+  
+    // Setup the textbox
+    // Clear the textbox
+    typedValueElement.value = '';
+    // set focus
+    typedValueElement.focus();
+    // set the event handler
+  
+    // Start the timer
+    startTime = new Date().getTime();
+  });
